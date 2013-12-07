3a5538a8a287f9128226fbd6032cb5b233cecdf4
Fethi El Hassasna <e.fethi.c@gmail.com>
2013-12-07 22:11:15 +0100

    Connect the ViewController to the model

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,18 +7,31 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic)Deck *deck;
 @end
 
 @implementation CardGameViewController
 
+- (Deck *)deck {
+    if (!_deck) {
+        _deck = [self createDeck];
+    }
+    return _deck;
+}
+
+- (Deck *)createDeck {
+    return [[PlayingCardDeck alloc] init];
+}
+
 - (void)setFlipCount:(int)flipCount {
     _flipCount = flipCount;
     self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
-    NSLog(@"Flips: %d", self.flipCount);
+    //NSLog(@"Flips: %d", self.flipCount);
 }
 
 - (IBAction)touchCardButton:(UIButton *)sender {
@@ -27,9 +40,12 @@
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
     } else {
-        [sender setBackgroundImage:[UIImage imageNamed:@"frontcard"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"frontcard"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+        }
     }
     self.flipCount++;
 }
