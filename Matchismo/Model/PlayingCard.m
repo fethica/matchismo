7f8aa9dbf87ead7bdddfb8d55dd7e8c8e95094dd
Fethi El Hassasna <e.fethi.c@gmail.com>
2013-12-08 21:42:00 +0100

    Override the Card match: method in PlayingCard class

--- Matchismo/Model/PlayingCard.m
+++ Matchismo/Model/PlayingCard.m
@@ -10,6 +10,22 @@
 
 @implementation PlayingCard
 
+- (int)match:(NSArray *)otherCards {
+    int score = 0;
+    
+    if ([otherCards count] == 1) {
+        PlayingCard *otherCard = [otherCards firstObject];
+        if (otherCard.rank == self.rank) {
+            score = 4;
+        } else if ([otherCard.suit isEqualToString:self.suit]) {
+            score = 1;
+        }
+    }
+    
+    
+    return score;
+}
+
 - (NSString *)contents {
     NSArray *rankStrings = [PlayingCard rankStrings];
     return [rankStrings[self.rank] stringByAppendingString:self.suit];
